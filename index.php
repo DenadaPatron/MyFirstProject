7966f01f304ec186eaef2cb657a6eed9cf26b4db
Inception <1408229@rgu.ac.uk>
2016-10-14 11:11:09 +0100

    nothing special

--- index.php
+++ index.php
@@ -8,16 +8,11 @@
 
 <?php
 
-for ($i = 1; $i < 31; $i++):
-     if($i % 2 == 0){
-         echo "On the" . $i . "day of the month specs are available";
-}
-     elseif($i % 3 == 0 ){
-        echo "On the" . $i . "day of the month mugs are available";
- }
-    elseif($i % 4 == 0){
-        echo "On the" . $i . "day of the month sausages are available";
+for($i = 1; $i <=30; $i++):
+    if($i%2==0){
+        echo "On the " . $i .  " day, specs are available";
     }
+
 
 
 
