4610d35698a9cac42d0a2ae4758a2d3a20757483
Inception <1408229@rgu.ac.uk>
2016-10-14 11:43:58 +0100

    nothing special

--- index.php
+++ index.php
@@ -12,35 +12,35 @@ for($i = 1; $i <=30; $i++):
 
     $ends = array('th','st','nd','rd','th','th','th','th','th','th');
     if (($i %100) >= 11 && ($i%100) <= 13)
-        $abbreviation = $i. 'th';
+        $a = $i. 'th';
     else
-        $abbreviation = $i. $ends[$i % 10];
+        $a = $i. $ends[$i % 10];
 
 
 
     if($i%2==0 && $i%3==0 && $i%4==0){
-        echo "On the " . $i . $abbreviation . " day, specs, mugs and sausage rolls are available" . "<br/> \n";
+        echo "On the " . $a .  " day, specs, mugs and sausage rolls are available" . "<br/> \n";
     }
     elseif($i%3==0 && $i%4==0){
-        echo "On the " . $i . " day, mugs and sausage rolls are available" . "<br/ \n>";
+        echo "On the " . $a . " day, mugs and sausage rolls are available" . "<br/ \n>";
     }
     elseif($i%2==0 && $i%4==0){
-        echo "On the " . $i . " day, specs and sausage rolls are available" . "<br/> \n";
+        echo "On the " . $a . " day, specs and sausage rolls are available" . "<br/> \n";
     }
     elseif($i%2==0 && $i%3==0) {
-        echo "On the " . $i . " day, specs and mugs are available" . "<br/> \n";
+        echo "On the " . $a . " day, specs and mugs are available" . "<br/> \n";
     }
     elseif($i%2==0){
-        echo " On the " . $i .  " day, specs are available" . "<br/>\n";
+        echo " On the " . $a .  " day, specs are available" . "<br/>\n";
     }
     elseif($i%3==0){
-        echo "On the " . $i . " day, mugs are available" . "<br/> \n";
+        echo "On the " . $a . " day, mugs are available" . "<br/> \n";
     }
     elseif($i%4==0){
-        echo "On the " . $i . " day, Sausage rolls are available" . "<br/> \n";
+        echo "On the " . $a. " day, Sausage rolls are available" . "<br/> \n";
     }
     else{
-        echo "On the " . $i . " day, nothing is available" . "<br/> \n";
+        echo "On the " . $a. " day, nothing is available" . "<br/> \n";
     }
 endfor;
 
