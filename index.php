15da72d9c2c5eee6614a8bb934d1d6382fa0fdc6
Inception <1408229@rgu.ac.uk>
2016-10-14 11:41:40 +0100

    nothing special

--- index.php
+++ index.php
@@ -9,6 +9,15 @@
 <?php
 
 for($i = 1; $i <=30; $i++):
+
+    $ends = array('th','st','nd','rd','th','th','th','th','th','th');
+    if (($i %100) >= 11 && ($i%100) <= 13)
+        $abbreviation = $i. 'th';
+    else
+        $abbreviation = $i. $ends[$i % 10];
+
+
+
     if($i%2==0 && $i%3==0 && $i%4==0){
         echo "On the " . $i . " day, specs, mugs and sausage rolls are available" . "<br/> \n";
     }
@@ -30,20 +39,9 @@ for($i = 1; $i <=30; $i++):
     elseif($i%4==0){
         echo "On the " . $i . " day, Sausage rolls are available" . "<br/> \n";
     }
-
-
-
-
-
-
-
-
-
-
     else{
         echo "On the " . $i . " day, nothing is available" . "<br/> \n";
     }
-
 endfor;
 
 
