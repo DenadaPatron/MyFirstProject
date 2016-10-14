3721115655a2fb7292053065064c3213aec66be4
Inception <1408229@rgu.ac.uk>
2016-10-14 11:15:26 +0100

    nothing special

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 
 for($i = 1; $i <=30; $i++):
     if($i%2==0){
-        echo "On the " . $i .  " day, specs are available";
+        echo "On the " . $i .  " day, specs are availabl \n";
 
     }
 
