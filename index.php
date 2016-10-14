c13d4387d5378369dc235c41ab31429e01db34ed
Inception <1408229@rgu.ac.uk>
2016-10-14 11:23:46 +0100

    nothing special

--- index.php
+++ index.php
@@ -10,9 +10,16 @@
 
 for($i = 1; $i <=30; $i++):
     if($i%2==0){
-        echo " \n On the " . $i .  " day, specs are available".  "<br/>\n";
-
-
+        echo " On the " . $i .  " day, specs are available" . "<br/>\n";
+    }
+    elseif($i%3==0){
+        echo "On the" . $i . " day, mugs are available" . "<br/> \n";
+    }
+    elseif($i%4==0){
+        echo "On the" . $i . " day, Sausage rolls are available" . "<br/> \n";
+    }
+    elseif($i%2==0 && $i%3==0) {
+        echo "On the" . $i . " day, specs and mugs are available" . "<br/> \n";
     }
 
 
